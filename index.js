fd3ed5cde8abcb00ad0eb52567ad32a190d07085
Aibek Ozhorov <ozhorov@gmail.com>
2019-07-05 03:13:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,42 +1,15 @@
-var animal = 'dog'
-
-function myAnimal() {
-  return animal
+function myAnimal(){
+  return "dog"
 }
-
-function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  return animal
+function yourAnimal(){
+  return "cat"
 }
-
-function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
-  const two = 2
+function add2(n){
+  return n+2
 }
-
-var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
+var funkyFunction = function() { // funkyFunction
+  return function() { // funkyFunction()
+    return "FUNKY!" // funkyFunction()()
   }
 }
-
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
-function theFunk () {
-  return "FUNKY!" }  ;
-  var x = "declared outside function";
-
-exampleFunction();
-
-function funkyFunction() {
-    console.log("Inside function");
-    console.log(x);
-}
-
-console.log("Outside function");
-console.log(x);
+var theFunk = funkyFunction()()
